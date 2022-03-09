5cdfd67362610d8419eda44a6c26ab8ebe92d2dc
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-03-08 20:53:44 -0500

    update using do while loop

--- LAB02/Program.cs
+++ LAB02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace COMP100SEC007_LAB02
 {
@@ -11,14 +12,13 @@ namespace COMP100SEC007_LAB02
         static void Main(string[] args)
         {  //variables
             int userInput;
-            bool condiction = true;
             double inches, centimeters, Fahrenheit, Celsius, a, b, c, discriminant = 0, x, x1;
 
             Console.WriteLine("Hello dear user, for this program you can select the following functions");
             Console.WriteLine();
 
             //while loop until condiction is false
-            while (condiction)
+            do
             {
                 //Display the menu option for user
                 Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
@@ -34,81 +34,76 @@ namespace COMP100SEC007_LAB02
                 userInput = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine();
 
-                //using if statement to check userinputs
-                if (userInput != 4)
+                //using swith statment to handle different user inputs
+                switch (userInput)
                 {
+                    case 1:
+                        //prompt for inches from user
+                        Console.Write("Please enter the measurement in Inches: ");
+                        inches = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine();
+                        //calculate the centimeters
+                        centimeters = inches * 2.54;
+                        //display output
+                        Console.WriteLine($"{inches} inches is equal to {centimeters} CM");
+                        break;
+                    case 2:
+                        //prompt for temperature in Fahrenheit
+                        Console.Write("Please enter the temperature in Fahrenheit: ");
+                        Fahrenheit = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine();
+                        //calculate degree Celsius
+                        Celsius = (Fahrenheit - 32) / 1.8;
+                        //display output
+                        //I googled that \x00B0 is the degree symbol
+                        Console.WriteLine($"{Fahrenheit}\x00B0F is equal to {Celsius:f2}\x00B0C");
+                        break;
+                    case 3:
+                        // prompt for the value of a b and c from user
+                        Console.Write("Please enter a for quadratic Equation: ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Please enter b for quadratic Equation: ");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Please enter c for quadratic Equation: ");
+                        c = Convert.ToDouble(Console.ReadLine());
 
-                    //using swith statment to handle different user inputs
-                    switch (userInput)
-                    {
-                        case 1:
-                            //prompt for inches from user
-                            Console.Write("Please enter the measurement in Inches: ");
-                            inches = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine();
-                            //calculate the centimeters
-                            centimeters = inches * 2.54;
-                            //display output
-                            Console.WriteLine($"{inches} inches is equal to {centimeters} CM");
-                            break;
-                        case 2:
-                            //prompt for temperature in Fahrenheit
-                            Console.Write("Please enter the temperature in Fahrenheit: ");
-                            Fahrenheit = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine();
-                            //calculate degree Celsius
-                            Celsius = (Fahrenheit - 32) / 1.8;
-                            //display output
-                            //I googled that \x00B0 is the degree symbol
-                            Console.WriteLine($"{Fahrenheit}\x00B0F is equal to {Celsius:f2}\x00B0C");
-                            break;
-                        case 3:
-                            // prompt for the value of a b and c from user
-                            Console.Write("Please enter a for quadratic Equation: ");
-                            a = Convert.ToDouble(Console.ReadLine());
-                            Console.Write("Please enter b for quadratic Equation: ");
-                            b = Convert.ToDouble(Console.ReadLine());
-                            Console.Write("Please enter c for quadratic Equation: ");
-                            c = Convert.ToDouble(Console.ReadLine());
+                        //importatnt condiction decideds the equation has two real roots or one or two distinct roots
+                        discriminant = (b * b) - (4 * a * c);
 
-                            //importatnt condiction decideds the equation has two real roots or one or two distinct roots
-                            discriminant = (b * b) - (4 * a * c);
-
-                            //using if statement to check discriminant
-                            if (discriminant > 0)
-                            {
-                                x = (-b + Math.Sqrt(b * b - 4 * a * c)) / (a * 2);
-                                x1 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (a * 2);
-                                Console.WriteLine($"X1 = {x}  X2 = {x1}");
-                            }
-                            else if (discriminant == 0)
-                            {
-                                x = (-b + Math.Sqrt(b * b - 4 * a * c)) / (a * 2);
-                                Console.WriteLine($"There is one real root x = {x}");
-                            }
-                            else
-                            {
-                                Console.WriteLine("This equation has two distinct(non-real) roots");
-                            }
-                            break;
-                        //user inputs number not in (1~4)
-                        default:
-                            Console.WriteLine("The number is out of range please try again");
-                            Console.WriteLine();
-                            continue;
-                    }
-                    Console.WriteLine("Press any key to continue");
-                    Console.ReadKey();
-                    Console.Clear();
+                        //using if statement to check discriminant
+                        if (discriminant > 0)
+                        {
+                            x = (-b + Math.Sqrt(b * b - 4 * a * c)) / (a * 2);
+                            x1 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (a * 2);
+                            Console.WriteLine($"X1 = {x}  X2 = {x1}");
+                        }
+                        else if (discriminant == 0)
+                        {
+                            x = (-b + Math.Sqrt(b * b - 4 * a * c)) / (a * 2);
+                            Console.WriteLine($"There is one real root x = {x}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("This equation has two distinct(non-real) roots");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("thank you for using");
+                        Console.ReadLine();
+                        return;
+                    //user inputs number not in (1~4)
+                    default:
+                        Console.WriteLine("The number is out of range please try again");
+                        Console.WriteLine();
+                        continue;
                 }
-                // when userinput == 4 
-                else
-                {
-                    //change loop condiction to exit
-                    condiction = false;
-                    Console.WriteLine("Thank you for using");
-                }
-            }
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
+            
+            } while (userInput != 4);
+           
+            
             Console.ReadLine();
         }
     }
